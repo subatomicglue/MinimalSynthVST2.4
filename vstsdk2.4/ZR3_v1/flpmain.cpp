3ca1a49d289b66e261c1da8b1c08a98130446e52
km <kmeinert@aviahealthinnovation.com>
2018-06-04 16:54:39 -0500

    remove vstsdk2.4 per steinberg DMCA takedown notice

--- vstsdk2.4/ZR3_v1/flpmain.cpp
+++
@@ -1,9 +0,0 @@
-#include "audioeffect.h"
-#include "Globals.h"
-#include "flp.h"
-
-//------------------------------------------------------------------------
-AudioEffect* createEffectInstance (audioMasterCallback audioMaster)
-{
-	return new flp (audioMaster);
-}
--- vstsdk2.4/ZR3_v1/readme.txt
+++
@@ -1,4 +0,0 @@
-sample application that came with vstlord (i think?)
-
-xcode project for MacOSX here.
-and dsp/dsw for win32.
